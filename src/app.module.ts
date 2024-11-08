e3ddb523d05245871e6bc8298308dfc9344ac8b4
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-11-08 16:40:20 +0100

    Added middleware and process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,36 @@
-import { Module } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './Common/CommonModule';
 import { ToDoModule } from './to-do/to-do.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 
 @Module({
-  imports: [CommonModule, ToDoModule,TypeOrmModule.forRoot({
-    type:'mysql',
-    host:'localhost',
-    port: 3306,
-    username:'nest_user',
-    password:'nest_pass',
-    database:'tp_nest',
-    synchronize:true,
-    autoLoadEntities:true
-  })],
+  imports: [
+    CommonModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes config accessible globally
+    }),
+    ToDoModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest_user',
+      password: 'nest_pass',
+      database: 'tp_nest',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('todo'); 
+  }
+}
