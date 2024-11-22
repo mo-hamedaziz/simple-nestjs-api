edd0e4b8d19e84fd3e60d8b483cd0e6b3be671b8
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-11-22 11:18:14 +0100

    Created DTOs

--- src/cv/dto/create-cv.dto.ts
+++ src/cv/dto/create-cv.dto.ts
@@ -1,1 +1,43 @@
-export class CreateCvDto {}
+/* eslint-disable prettier/prettier */
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  IsString,
+  IsArray,
+} from 'class-validator';
+
+export class CreateCvDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  firstname: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
+
+  @IsNotEmpty()
+  @IsString()
+  Cin: string;
+
+  @IsNotEmpty()
+  @IsString()
+  Job: string;
+
+  @IsNotEmpty()
+  @IsString()
+  path: string;
+
+  @IsOptional()
+  @IsNumber()
+  userId?: number;
+
+  @IsOptional()
+  @IsArray()
+  @IsNumber({}, { each: true })
+  skills?: number[];
+}
--- src/cv/dto/update-cv.dto.ts
+++ src/cv/dto/update-cv.dto.ts
@@ -1,4 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCvDto } from './create-cv.dto';
+import { IsOptional, IsNumber, IsArray } from 'class-validator';
 
-export class UpdateCvDto extends PartialType(CreateCvDto) {}
+export class UpdateCvDto extends PartialType(CreateCvDto) {
+  @IsOptional()
+  @IsNumber()
+  userId?: number;
+
+  @IsOptional()
+  @IsArray()
+  @IsNumber({}, { each: true })
+  skills?: number[];
+}
